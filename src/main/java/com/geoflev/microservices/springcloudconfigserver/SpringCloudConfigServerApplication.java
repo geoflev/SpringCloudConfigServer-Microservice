68227769f8ac361264f26f4081fe3acbc1c9dad2
GeoFlev <geoflev@hotmail.com>
2020-08-27 13:08:56 +0300

    first commit

---
+++ src/main/java/com/geoflev/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.geoflev.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+//without enable im getting404
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
